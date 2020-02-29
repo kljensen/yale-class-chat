e62c9d6c540dee3e98aa159038cdde45fb392731
Nick Peranzi <nick@DESKTOP-P47TRKU.kingperanzi>
2020-02-29 18:07:52 -0500

    Add fields to hide submissions and allow ranking

--- app/lib/app/submissions/submission.ex
+++ app/lib/app/submissions/submission.ex
@@ -7,10 +7,13 @@ defmodule App.Submissions.Submission do
     field :image_url, :string
     field :slug, :string
     field :title, :string
+    field :allow_ranking, :boolean, default: false
+    field :hidden, :boolean, default: false
     belongs_to :topic, App.Topics.Topic
     belongs_to :user, App.Accounts.User
     has_many :comments, App.Submissions.Comment
     has_many :ratings, App.Submissions.Rating
+
 
     timestamps()
   end
@@ -18,8 +21,8 @@ defmodule App.Submissions.Submission do
   @doc false
   def changeset(submission, attrs) do
     submission
-    |> cast(attrs, [:title, :description, :slug, :image_url])
-    |> validate_required([:title, :description, :slug])
+    |> cast(attrs, [:title, :description, :slug, :image_url, :allow_ranking, :hidden])
+    |> validate_required([:title, :description, :slug, :allow_ranking, :hidden])
     |> unique_constraint(:slug)
     |> foreign_key_constraint(:topic_id)
     |> assoc_constraint(:topic)
--- app/priv/repo/migrations/20200215195954_create_topics.exs
+++ app/priv/repo/migrations/20200215195954_create_topics.exs
@@ -12,6 +12,8 @@ defmodule App.Repo.Migrations.CreateTopics do
       add :allow_submission_voting, :boolean, default: false, null: false
       add :anonymous, :boolean, default: false, null: false
       add :allow_submission_comments, :boolean, default: false, null: false
+      add :allow_ranking, :boolean, default: false, null: false
+      add :show_user_submissions, :boolean, default: false, null: false
       add :user_submission_limit, :integer
       add :sort, :string
       add :section_id, references(:sections, on_delete: :delete_all), null: false
