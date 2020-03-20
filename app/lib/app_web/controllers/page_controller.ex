ce437087b08689f73dbb58a94ef01b6251caa811
Nick Peranzi <nperanzi@gmail.com>
2020-03-20 17:10:05 -0400

    Adjust layout to show sections to students and courses to faculty

--- app/lib/app_web/controllers/page_controller.ex
+++ app/lib/app_web/controllers/page_controller.ex
@@ -1,5 +1,8 @@
 defmodule AppWeb.PageController do
   use AppWeb, :controller
+  alias App.Courses
+  alias App.Courses.Course
+  alias App.Courses.Section
 
   def index(conn, _params) do
     case get_session(conn, :uid) do
@@ -7,15 +10,32 @@ defmodule AppWeb.PageController do
         render(conn, "index.html", [current_user: nil, is_faculty: false])
 
       uid ->
-        case App.Accounts.get_user_by(uid) do
+        case IO.inspect(App.Accounts.get_user_by(uid)) do
           nil ->
             conn
             |> configure_session(drop: true)
             |> render("index.html", [current_user: nil, is_faculty: false])
 
-          current_user ->
-            is_faculty = current_user.is_faculty
-            render(conn, "index.html", [current_user: current_user, is_faculty: is_faculty])
+          user ->
+            conn = conn
+                    |> assign(:current_user, user)
+                    |> assign(:user_signed_in?, true)
+
+            case user.is_faculty do
+              true ->
+                courses = Courses.list_user_courses(user)
+                list = Courses.list_semester_names()
+                semesters = Map.new(Enum.map(list, fn [key, value] -> {:"#{key}", value} end))
+                conn
+                |> put_view(AppWeb.CourseView)
+                |> render("index.html", [courses: courses, semesters: semesters])
+
+              false ->
+                sections = Courses.list_user_sections(user)
+                conn
+                |> put_view(AppWeb.UserSectionView)
+                |> render("index.html", sections: sections)
+              end
           end
       end
   end
