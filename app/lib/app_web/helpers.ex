3bc5886514765cddc658a5230197fb18faa8ee50
Kyle L. Jensen <kljensen@gmail.com>
2020-03-20 14:01:32 -0400

    Improve display of ratings

--- app/lib/app_web/helpers.ex
+++ app/lib/app_web/helpers.ex
@@ -1,4 +1,5 @@
 defmodule AppWeb.Helpers do
+  require Decimal
 
   def truncate_text(string, num) do
     if String.length(string) > num + 3 do
@@ -12,4 +13,24 @@ defmodule AppWeb.Helpers do
     {:ok, curtime} = DateTime.now("Etc/UTC")
     curtime
   end
+
+  def stars(nil), do: ""
+  def stars(0), do: ""
+  def stars(num) do
+    whole_stars = Decimal.to_float(num) |> trunc
+    fractional_stars = Decimal.to_float(num) - whole_stars
+    fraction_string = cond do
+        fractional_stars >= 0.75 ->
+            "¾"
+        fractional_stars >= 0.50 ->
+            "½"
+        fractional_stars >= 0.25 ->
+            "¼"
+        true ->
+            ""
+    end
+    String.duplicate("★", whole_stars) <> fraction_string
+
+  end
+
 end
