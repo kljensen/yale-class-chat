d0bc742441c5da617720ce1266eab935631bd735
Kyle L. Jensen <kljensen@gmail.com>
2020-03-20 21:03:57 -0400

    Truncate long descriptions where they look weird

--- app/lib/app_web/helpers.ex
+++ app/lib/app_web/helpers.ex
@@ -42,4 +42,20 @@ defmodule AppWeb.Helpers do
      Regex.split(~r{\n\s*\n}, text, trim: true)
   end
 
+  def truncate_text(text, max_words, max_chars) do
+    text_normalized = Regex.replace(~r/[ \t]+/u, text,  " ")
+    # Split on whitespace but keep whitespace
+    text_split = Regex.split(~r/(\s+)/u, text_normalized, include_captures: true)
+    words_kept = Enum.slice(text_split, 0, max_words)
+    truncated_words = length(text_split) != length(words_kept)
+
+    words_kept_joined = Enum.join(words_kept, "")
+    words_kept_joined_sliced = String.slice(words_kept_joined, 0, max_chars)
+    truncated_string = String.length(words_kept_joined) != String.length(words_kept_joined_sliced)
+    paragraphs = words_kept_joined_sliced |> split_newlines
+    was_truncated = truncated_words ||  truncated_string
+    {was_truncated, paragraphs}
+  end
+  
+
 end
