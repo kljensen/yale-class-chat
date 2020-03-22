da79d0eb019c1f0de95785ea003b8e5ccfe74405
Nick Peranzi <nperanzi@gmail.com>
2020-03-21 22:30:41 -0400

    Working on fixing TA view - currently shows all sections

--- app/lib/app/courses.ex
+++ app/lib/app/courses.ex
@@ -463,13 +463,19 @@ defmodule App.Courses do
               on: r.section_id == s.id,
               left_join: c in Course,
               on: s.course_id == c.id,
+              left_join: cr in App.Accounts.Course_Role,
+              on: cr.course_id == c.id,
               left_join: se in Semester,
               on: c.semester_id == se.id,
-              where: r.user_id == ^uid,
-              where: r.valid_from <= from_now(0, "day"),
-              where: r.valid_to >= from_now(0, "day"),
+              where: (r.user_id == ^uid and r.valid_from <= from_now(0, "day") and r.valid_to >= from_now(0, "day")) or (cr.user_id == ^uid and cr.valid_from <= from_now(0, "day") and cr.valid_to >= from_now(0, "day")),
               where: c.allow_read == true,
-              select: %{id: s.id, title: s.title, crn: s.crn, course_num: c.number, course_dept: c.department, course_name: c.name, semester_name: se.name}
+              select: %{id: s.id,
+                title: s.title,
+                crn: s.crn,
+                course_num: c.number,
+                course_dept: c.department,
+                course_name: c.name,
+                semester_name: se.name}
     Repo.all(query)
   end
 
