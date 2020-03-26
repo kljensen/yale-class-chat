896499db700d4857b2f9a7d26c3d8a4689e836d3
Kyle L. Jensen <kljensen@gmail.com>
2020-03-26 10:33:44 -0400

    Use old template as basis for liveview of topics

--- app/lib/app_web/live/topic_live.ex
+++ app/lib/app_web/live/topic_live.ex
@@ -7,15 +7,29 @@ defmodule AppWeb.TopicLive do
 
 
   def render(assigns) do
-    ~L"""
-    Current temperature: <%= @temperature %> <br>
-    Topic title = **<%= @topic.title %>**<br>
-    """
+    Phoenix.View.render(AppWeb.TopicView, "live.html", assigns)
   end
 
   defp subscribe(model, id) do
     notification_topic = App.Notifications.topic_for_model(model)
     Phoenix.PubSub.subscribe(App.PubSub, notification_topic)
+  end
+
+
+  @doc """
+  Load topic data into the assigns. Gets net_id and topic id
+  from the socket assigns.
+  """
+  defp load_topic_data(%{"uid" => net_id, "id" => id} = socket) do
+    load_topic_data(socket, net_id, id)
+  end
+
+  @doc """
+  Load topic data into the assigns for this net_id and topic id.
+  """
+  defp load_topic_data(socket, net_id, id) do
+    topic_data = App.Topics.get_topic_data_for_net_id(net_id, id)
+    assign(socket, topic_data)
   end
 
   @doc """
@@ -26,16 +40,24 @@ defmodule AppWeb.TopicLive do
   The first argument is the URL params. The second are
   the session variables, and the last is the socket.
   """
-  def mount(%{"id" => id}, %{"uid" => uid}, socket) do
+  def mount(%{"id" => id}, %{"uid" => net_id}, socket) do
     if connected?(socket) do
       :ok = subscribe(Topic, id)
     end
-    topic_data = App.Topics.get_topic_data_for_net_id(uid, id)
     temperature = 50
     socket = socket 
-    |> assign(topic_data)
     |> assign(:id, id)
     |> assign(:temperature, temperature)
+    |> assign(:conn, AppWeb.Endpoint)
+    |> load_topic_data(net_id, id)
+
+    # Here, I fake the conn based on the suggestion in github:
+    # https://github.com/phoenixframework/phoenix_live_view/issues/277
+    # It seems to work but doesn't feel right. Mostly this is to get
+    # `Routes.topic_submission_path(@conn, :new, @topic.id)` to work.
+    # I think that's the only way in which I'm depending on the @conn.
+    # TODO: refactor. One easy way is to pre-load all the routes we
+    # need into assigns. Another, I should look at
 
     Logger.info("\n\n")
     socket.assigns
@@ -55,11 +77,11 @@ defmodule AppWeb.TopicLive do
     {:noreply, assign(socket, :temperature, temperature)}
   end
   def handle_info(action, socket) do
-    Logger.info(">>>>woot")
+    Logger.info("\n\n\n>>>>Received a pg update for topic")
     action
     |> inspect()
     |> Logger.info()
-    Logger.info("<<<<woot")
+    Logger.info("<<<<\n\n\n")
     temperature = socket.assigns.temperature + 1
     {:noreply, assign(socket, :temperature, temperature)}
   end
